d694f1d219ea5dfa56f740222a62597c98d9166c
adamwasiak <adam.wasiak82@gmail.com>
2021-11-21 12:15:48 +0000

    Add consts and eventlistener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,40 @@
+const choices = document.querySelectorAll(".choices");
+let playerScore = 0;
+let computerScore = 0;
+
+choices.forEach(choice) => {
+    choice.addEventListener("click", function() {
+        const playerInput = this.value;
+
+        const computerChoices(playerInput, computerInput);
+        compareInputs(playerInput, computerInput);
+        updateScore();
+        if(checkWinner()) {
+            playerScore = computerScore = 0;
+            updateScore();
+        }
+    });
+});
+
+// function to update game selections
 function updateSelections(playerInput, computerInput){
 
 }
+
+// function to compare game inputs
 
 function compareInputs(playerInput, computerInput){
 
 }
 
+// function to update scores 
+
 function updateScore() {
 
 }
 
+// function to check for game winner
+
 function checkWinner () {
-    
+
 }
