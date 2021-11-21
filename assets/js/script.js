2cb89b3e0102377c30888ee76049e10d449bd678
adamwasiak <adam.wasiak82@gmail.com>
2021-11-21 12:04:48 +0000

    Change input class name and add initial js function placeholders

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function updateSelections(playerInput, computerInput){
+
+}
+
+function compareInputs(playerInput, computerInput){
+
+}
+
+function updateScore() {
+
+}
+
+function checkWinner () {
+    
+}
